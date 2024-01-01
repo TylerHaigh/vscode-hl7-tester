1516e8675a08efd39f9cdfe580d6856f12b6fd52
Tyler Haigh <tylerhaigh1994@me.com>
2024-01-01 14:43:26 +1100

    Initial commit. Draft of HL7 message tester extension

---
+++ src/extension.ts
@@ -0,0 +1,184 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { sendOneShot } from './one-shot-connection';
+
+
+export interface VSCodeMessage {
+	command: 'alert'
+	text: string
+	payload: {
+		hl7: string
+		server: string
+		port: string
+	}
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "myfirstextension" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const helloWorld = vscode.commands.registerCommand('myfirstextension.helloWorld.custom', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from myfirstextension with edits!!');
+	});
+
+	const displayCurrentTime = vscode.commands.registerCommand('myfirstextension.currentTime', () => {
+		const now = new Date();
+		vscode.window.showWarningMessage('Current time is ' + now.toString())
+	});
+
+	const catCoding = vscode.commands.registerCommand('catCoding.start', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'catCoding', // Identifies the type of the webview. Used internally
+			'Cat Coding', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{
+				enableScripts: true
+			  } // Webview options. More on these later.
+
+		);
+
+		function getWebviewContent() {
+			return `<!DOCTYPE html>
+		  <html lang="en">
+		  <head>
+			  <meta charset="UTF-8">
+			  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+			  <title>Cat Coding</title>
+
+			  <style>
+
+			  	.hl7-text-box {
+					width:100%;
+					 white-space: pre;
+					overflow-wrap: normal;
+					overflow-x: scroll;
+				}
+			  </style>
+		  </head>
+		  <body>
+
+		  		<h1>Message Editor</h1>
+				<label for="hl7MessageEditor">HL7 Message</label> 
+				<textarea id="hl7MessageEditor" rows="10" class="hl7-text-box"></textarea>
+
+				<hr />
+
+				<h1>HL7 Connector Config</h1>
+				<label for="server">Server Host</label> 
+				<input type="text" id="server" value="localhost" />
+
+				<label for="port">Port</label> 
+				<input type="text" id="port"  value="8081"/>
+
+				<hr />
+
+				<input type="button" onclick="sendHl7Message()" value="Send" style="width:100%;"></input>
+
+				<hr />
+
+				<h1>HL7 Response</h1>
+				<textarea id="hl7Response" rows="10" readonly="true" class="hl7-text-box" ></textarea>
+
+
+			
+				<script>
+
+
+				// Handle the message inside the webview
+				window.addEventListener('message', event => {
+		
+					const message = event.data; // The JSON data our extension sent
+		
+					switch (message.command) {
+						case 'responseData': {
+							const response = document.getElementById('hl7Response');
+
+							const data = message.data;
+							response.value = data;
+						}
+							
+					}
+				});
+
+
+				function loadVSCodeApiOnlyOnce() {
+					// https://stackoverflow.com/a/62968179
+					// As per stack overflow link above
+					// Only load the VSCode api once. Doing so multiple times
+					// will result in hidden failures.
+
+					if (!window.vscode) {
+						const vscode = acquireVsCodeApi();
+						window.vscode = vscode;
+					}
+
+					return window.vscode;
+				}
+
+				function sendHl7Message() {
+
+					const vscode = loadVSCodeApiOnlyOnce();
+					
+					const hl7 = document.getElementById('hl7MessageEditor').value;
+					const server = document.getElementById('server').value;
+					const port = document.getElementById('port').value;
+
+					vscode.postMessage({
+                        command: 'alert',
+						text: '🐛  on line 1' ,
+						payload: {hl7, server, port}
+                    })
+
+					return false;
+				}
+				</script>
+				
+
+		  </body>
+		  </html>`;
+		  }
+		
+		// And set its HTML content
+		panel.webview.html = getWebviewContent();
+
+		 // Handle messages from the webview
+		 panel.webview.onDidReceiveMessage(
+			(message: VSCodeMessage) => {
+			  switch (message.command) {
+				case 'alert': {
+					
+					const details = message.payload;
+					if (!details) { return; }
+
+					if (!details.server || !details.port || !details.hl7) {
+						vscode.window.showWarningMessage(`No message sent. missing details.`);
+						return;
+					}
+
+					vscode.window.showInformationMessage(`Sending HL7 message to ${details.server}:${details.port}`);
+					sendOneShot({ host: details.server, port: parseInt(details.port)}, details.hl7, panel);
+					return;
+				}
+			  }
+			},
+			undefined,
+			context.subscriptions
+		  );
+	});
+
+	context.subscriptions.push(helloWorld);
+	context.subscriptions.push(displayCurrentTime);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
---
+++ src/one-shot-connection.ts
@@ -0,0 +1,58 @@
+import net from 'net';
+import * as vscode from 'vscode';
+
+export interface HL7ConnectionDetails {
+    host: string
+    port: number
+}
+
+const VT = String.fromCharCode(0x0b);
+const FS = String.fromCharCode(0x1c);
+const CR = String.fromCharCode(0x0d);
+
+export function sendOneShot(conn: HL7ConnectionDetails, message: string, panel: vscode.WebviewPanel) {
+
+    const payload = VT + message + FS + CR;
+
+    const socket = net.createConnection({
+        host: conn.host,
+        port: conn.port
+    });
+
+    socket.on('connect', () => {
+        console.log('Socket has connected!');
+    });
+
+    socket.on('close', (hadError: boolean) => {
+        console.log('Socket has closed' + (hadError ? ' with error' : ''));
+    });
+
+    socket.on('error', (err) => {
+        console.error('Error detected: ' + err.message);
+    });
+
+    socket.on('data', (data: Buffer) => {
+        let response = data.toString('utf8');
+        
+        response = response.replace(/[\r\n]+/g, '\n');
+        
+        console.log(response);
+
+        // Send a message to our webview.
+        // You can send any JSON serializable data.
+        panel.webview.postMessage({ command: 'responseData', data: response });
+    });
+
+    socket.on('end', () => {
+        console.log('Socket has ended!');
+    });
+
+    socket.write(payload, (err) => {
+        if (err) {
+            vscode.window.showErrorMessage(err.message);
+        }
+
+        socket.end();
+    });
+
+}
